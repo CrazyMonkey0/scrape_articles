46d56593b03bb18c9669345fb7f1970312921ca7
CrazyMonkey0 <hubcio1765@gmail.com>
2025-10-16 15:07:13 +0200

    Add Article model

--- just_scrape/admin.py
+++ just_scrape/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'url')
+    search_fields = ('title', 'content')
+    list_filter = ('published_date',)
+    fields = ('title', 'content', 'content_html', 'url', 'published_date')
--- just_scrape/models.py
+++ just_scrape/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    content_html = models.TextField()
+    url = models.URLField(unique=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
