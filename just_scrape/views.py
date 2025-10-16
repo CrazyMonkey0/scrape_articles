4f5c2c968323a3d5e5f6e1c5be556f6d3caf1a63
CrazyMonkey0 <hubcio1765@gmail.com>
2025-10-16 16:11:07 +0200

    Filter articles by source domain in URL

--- just_scrape/views.py
+++ just_scrape/views.py
@@ -6,3 +6,10 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all().order_by('-published_date')
     serializer_class = ArticleSerializer
     
+    # Filtracja według źródła (domeny) w URL
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        source = self.request.query_params.get("source")
+        if source:
+            queryset = queryset.filter(url__icontains=source)
+        return queryset
