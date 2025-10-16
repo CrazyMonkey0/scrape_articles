51b618d0aeae7a85cb43fb26a0e0417f3778c3b2
CrazyMonkey0 <hubcio1765@gmail.com>
2025-10-16 14:04:27 +0200

    Add article scraping and date parsing with Playwright

---
+++ scraper.py
@@ -0,0 +1,81 @@
+from playwright.sync_api import sync_playwright
+from datetime import datetime, timedelta
+
+
+#Słownik z datami
+dates = {'stycznia': 1, 'lutego': 2, 'marca': 3, 'kwietnia': 4,
+    'maja': 5, 'czerwca': 6, 'lipca': 7, 'sierpnia': 8,
+    'września': 9, 'października': 10, 'listopada': 11, 'grudnia': 12,}
+
+urls = ["https://galicjaexpress.pl/ford-c-max-jaki-silnik-benzynowy-wybrac-aby-zaoszczedzic-na-paliwie", 
+       "https://galicjaexpress.pl/bmw-e9-30-cs-szczegolowe-informacje-o-osiagach-i-historii-modelu", 
+       "https://take-group.github.io/example-blog-without-ssr/jak-kroic-piers-z-kurczaka-aby-uniknac-suchych-kawalkow-miesa", 
+       "https://take-group.github.io/example-blog-without-ssr/co-mozna-zrobic-ze-schabu-oprocz-kotletow-5-zaskakujacych-przepisow" ]
+
+def parse_date(date_str):
+    
+    # Zamiana polskich nazw miesięcy na liczby
+       
+    for month_name, month_number in dates.items():
+        if month_name in date_str:
+            date_str = date_str.replace(month_name, str(month_number))
+            break
+
+    # Obsługa dat (ago)
+    if 'ago' in date_str:
+        parts = date_str.split()
+        value = int(parts[0])
+        unit = parts[1]
+        
+        if 'second' in unit:
+            date_str = (datetime.now() - timedelta(seconds=value)).strftime('%d.%m.%Y, %H:%M:%S')
+        elif 'minute' in unit:
+            date_str = (datetime.now() - timedelta(minutes=value)).strftime('%d.%m.%Y, %H:%M:%S')
+        elif 'hour' in unit:
+            date_str = (datetime.now() - timedelta(hours=value)).strftime('%d.%m.%Y, %H:%M:%S')
+        elif 'day' in unit:
+            date_str = (datetime.now() - timedelta(days=value)).strftime('%d.%m.%Y, %H:%M:%S')
+        elif 'week' in unit:
+            date_str = (datetime.now() - timedelta(weeks=value)).strftime('%d.%m.%Y, %H:%M:%S')
+        elif 'month' in unit:
+            date_str = (datetime.now() - timedelta(days=30*value)).strftime('%d.%m.%Y, %H:%M:%S')
+        elif 'year' in unit:
+            date_str = (datetime.now() - timedelta(days=365*value)).strftime('%d.%m.%Y, %H:%M:%S')
+    else:
+        # Obsługa dat w formacie "5 02 2023" lub 5.02.2023"
+        if '.' not in date_str:
+            date_str = date_str.replace(' ', '.') + ', 00:00:00'
+        elif ',' not in date_str:
+            date_str = date_str + ', 00:00:00'
+        
+    # Parsowanie daty
+    date_obj = datetime.strptime(date_str, '%d.%m.%Y, %H:%M:%S')
+    print(date_obj)
+    return date_obj
+
+with sync_playwright() as playwright:
+    browser = playwright.firefox.launch(headless=True)  
+    page = browser.new_page()
+
+    for url in urls:
+        page.goto(url)
+        # Tytuł
+        title = page.title()
+        print(f'Tytuł: {title}')
+        # Artykuł z tagami html
+        orginal_article = page.inner_html('article')
+        print(f'Artykuł z tagami html: {orginal_article}')
+        # Artykuł bez tagów html
+        text_article = page.inner_text('article')
+        print(f'Artykuł bez tagów html: {text_article}')
+        # URL
+        print(f'URL: {url}')
+        # Data publikacji
+        date_article = page.inner_text('time') if page.locator('time').count() > 0 else page.inner_text('article div p')
+        date_article = parse_date(date_article)
+        # Parsowanie daty
+        print(f'Data publikacji: {date_article}')
+    browser.close()
+    
+    
+    
