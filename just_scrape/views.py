c248497cfc03a991812ce04daa2d130196afbfdb
CrazyMonkey0 <hubcio1765@gmail.com>
2025-10-16 16:09:52 +0200

    Return all articles and a single article by identifier

--- just_scrape/views.py
+++ just_scrape/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .serializers import ArticleSerializer
+from .models import Article
+from rest_framework import viewsets
 
-# Create your views here.
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all().order_by('-published_date')
+    serializer_class = ArticleSerializer
+    
